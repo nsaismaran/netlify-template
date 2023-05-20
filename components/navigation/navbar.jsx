f6861a85bfff1286fb3e52148b7f45ab4b0702a1
smaran <nsaismaran@gmail.com>
2023-05-20 09:59:43 -0400

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="https:/edhelper.netlifyapp/?a=create-web3-dapp" target={"_blank"}>
 				<img
 					className={styles.alchemy_logo}
 					src="/alchemy_logo.svg"
